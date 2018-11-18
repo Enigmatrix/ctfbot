f49d28a36a8cfe74d7c91a2c2279e54b9ed4575c
Enigmatrix <enigmatrix2000@gmail.com>
2018-11-18 23:32:38 +0800

    commands

--- src/bot.ts
+++ src/bot.ts
@@ -1,9 +1,16 @@
 import { Client } from 'discord.js';
 import logger from './logger';
+import commands from './commands';
 
 const bot = new Client();
 bot.on("ready", () => {
     logger.info('CTFBot Ready');
+});
+bot.on('message', msg => {
+    if(msg.content[0] !== "!") return;
+
+    let [cmd, ...args] = msg.content.substr(1).split(' ');
+    commands.run(cmd, { args, msg });
 });
 
 export default bot;
--- src/commands.ts
+++ src/commands.ts
@@ -1,1 +1,53 @@
 import logger from './logger';
+import { Message } from 'discord.js';
+import { stringify } from 'querystring';
+
+declare type RunArgs= { args: string[], msg: Message};
+declare type RunMethod = (a:RunArgs) => Promise<void>
+
+export class Command {
+    public name: string;
+    public _description: string | undefined;
+    public _usage: string | undefined;
+    public run: RunMethod;
+
+    constructor(name: string, run: RunMethod){
+        this.name = name;
+        this.run = run;
+    }
+
+    description(description: string){
+        this._description = description;
+        return this;
+    }
+    usage(usage: string){
+        this._usage = usage;
+        return this;
+    }
+}
+
+class Commands {
+    private commandMap: Map<string, Command>;
+
+    constructor(){
+        this.commandMap = new Map<string, Command>();
+    }
+
+    register(cmd: Command): Commands{
+        this.commandMap.set(cmd.name, cmd);
+        return this;
+    }
+
+    run(name: string, runArgs: RunArgs){
+        let command = this.commandMap.get(name);
+        if(!command){
+            logger.warn(`Unrecognized command: ${name} (${runArgs.msg.content})`);
+            return;
+        }
+        logger.info(`Running command ${name} (${runArgs.msg.content})`);
+        command.run(runArgs)
+            .catch(e => logger.error(`Unexpected error while running ${name}`, e));
+    }
+}
+
+export default new Commands();
