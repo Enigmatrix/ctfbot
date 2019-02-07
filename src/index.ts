d532d10d4242bd209865b0a80bf16d4a9a803549
Enigmatrix <enigmatrix2000@gmail.com>
2019-02-07 20:49:45 +0800

    -_-

--- src/index.ts
+++ src/index.ts
@@ -4,17 +4,10 @@ import bot from "./bot";
 import {initConnection} from "./data";
 import { config } from "./util";
 
-import * as CTF from './ctftime';
-(async () => {
-  let writeups = await CTF.getLatestWriteups()
-  console.log(writeups)
-})();
 
-/*
 // If you want config.json so that you can test the bot too, contact me.
 (async () => {
     await initConnection();
     await agenda.start();
     await bot.login(config("DISCORD_BOT_TOKEN"));
 })();
-*/
