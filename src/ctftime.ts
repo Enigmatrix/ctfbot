19246a9019cdadfb518ae96cddc9a8b62439da52
Enigmatrix <enigmatrix2000@gmail.com>
2019-05-17 14:58:30 +0800

    not needed

--- src/ctftime.ts
+++ src/ctftime.ts
@@ -2,7 +2,6 @@ import axios from "axios";
 import logger from "./logger";
 import Parser from 'rss-parser';
 import cheerio from 'cheerio';
-import * as HttpsProxyAgent from 'https-proxy-agent';
 
 axios.defaults.headers.common['User-Agent'] = "Mozilla/5.0 (Linux; Android 4.0.4; Galaxy Nexus Build/IMM76B) AppleWebKit/535.19 (KHTML, like Gecko) Chrome/18.0.1025.133 Mobile Safari/535.19";
 
