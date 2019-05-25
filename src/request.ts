43be06891796b35a5d3f0ec0a53863a968b5acdb
Enigmatrix <enigmatrix2000@gmail.com>
2019-05-25 12:18:30 +0800

    proper proxy

--- src/request.ts
+++ src/request.ts
@@ -1,18 +1,23 @@
 import axios, { AxiosProxyConfig } from "axios-https-proxy-fix";
+import { chooseRandom } from "./util";
 const ProxyList = require("proxy-sources");
-import { chooseRandom } from './util';
 
 axios.interceptors.request.use(async config => {
-    const proxyAddrs:string[] = await ProxyList();
-    const chosenProxyAddrParts = chooseRandom(proxyAddrs).split(":");
-    const chosenProxy : AxiosProxyConfig = {
-        host: chosenProxyAddrParts[0],
-        port: +chosenProxyAddrParts[1]
-    };
-    console.log(`Using proxy ${chosenProxyAddrParts} for ${config.url}`);
-    config.proxy = chosenProxy;
-    return config;
+  let proxyAddrs = { list: [], random: () => "" };
+  do {
+    proxyAddrs = await ProxyList({
+      checker: true,
+      timeout: 1e3
+    });
+  } while (proxyAddrs.list.length === 0);
+  const chosenProxyAddrParts = proxyAddrs.random().split(":");
+  const chosenProxy: AxiosProxyConfig = {
+    host: chosenProxyAddrParts[0],
+    port: +chosenProxyAddrParts[1]
+  };
+  console.log(`Using proxy ${chosenProxyAddrParts} for ${config.url}`);
+  config.proxy = chosenProxy;
+  return config;
 });
 
 export default axios;
-
