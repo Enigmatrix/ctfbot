3b544e1aea4f5ecc4c15380ab5722e7797507a58
Enigmatrix <enigmatrix2000@gmail.com>
2019-06-17 00:35:41 +0800

    mwha

--- src/server.ts
+++ src/server.ts
@@ -1,25 +1,37 @@
 import fastify from "fastify";
-import {initConnection} from './data';
-import {Resource} from "./entities/resource";
-import {config} from "./util";
+import cors from "fastify-cors";
+import { getMongoRepository } from "typeorm";
+import { initConnection } from "./data";
+import { Resource } from "./entities/resource";
+import { config } from "./util";
 
-const app = fastify({logger: true});
+const app = fastify({ logger: true });
 
-app.get("/api/resources/:category", async (request, reply) => {
-    return (await Resource.find({category: request.params.category}))
-        .map((x) => { const {id, ...obj} = x; return obj; });
+app.register(cors, {
+  origin: false,
+});
+
+app.get("/api/resources/categories", async (request, reply) => {
+    return getMongoRepository(Resource).distinct("category", {});
+});
+
+app.get("/api/resources/by_category/:category", async (request, reply) => {
+  return (await Resource.find({ category: request.params.category })).map((x) => {
+    const { id, ...obj } = x;
+    return obj;
+  });
 });
 
 // Run the server!
 const setupServer = async () => {
   try {
-    await app.listen(config("PORT"), '0.0.0.0');
+    await app.listen(config("PORT"), "0.0.0.0");
   } catch (err) {
     app.log.error(err);
   }
 };
 
 (async () => {
-    await initConnection();
-    setupServer();
+  await initConnection();
+  setupServer();
 })();
