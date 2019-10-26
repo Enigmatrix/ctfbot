4dccb75de894feb390d4ca8c64d0c97980bc57cc
Enigmatrix <enigmatrix2000@gmail.com>
2019-10-26 14:39:05 +0800

    development logger vs production logger

--- src/utils/logger.ts
+++ src/utils/logger.ts
@@ -1,9 +1,23 @@
-import {createLogger, format, transports} from "winston";
+import {createLogger, format, Logger, transports} from "winston";
 
-export default createLogger({
-  format: format.combine(
-    format.errors({stack: true}),
-    format.json()),
-  // TODO: use the winston Discord transport
-  transports: [new transports.Console()],
-});
+let logger: Logger;
+if ((process.env.NODE_ENV || 'dev') === 'production') {
+  logger =  createLogger({
+    format: format.combine(
+      format.errors({stack: true}),
+      format.json()),
+    // TODO: use the winston Discord transport
+    transports: [new transports.Console()],
+  });
+}
+else {
+  logger = createLogger({
+    format: format.combine(
+      format.errors({stack: true}),
+      format.colorize(),
+      format.simple()),
+    transports: [new transports.Console()],
+  });
+}
+
+export default logger;
