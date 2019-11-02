5825b83f8f2aa7e4fffd41f7365a9088b58a60f8
Enigmatrix <enigmatrix2000@gmail.com>
2019-11-03 00:50:38 +0800

    add creds and archive

--- src/commands/ctf.ts
+++ src/commands/ctf.ts
@@ -1,9 +1,10 @@
 import { TextChannel } from "discord.js";
 import { CTFTimeCTF } from "../db/entities/ctf";
-import { ctfMainEmbed } from "../services/ctf";
+import { ctfMainEmbed, getCtfMainEmbed, getCTFTimeCTF } from "../services/ctf";
 import * as CTFTime from "../services/ctftime";
 import { Board, Color, Label } from "../services/trello";
 import { isCTFTimeUrl } from "../utils";
+import { CommandError, success } from "../utils/message";
 import { CmdCtx, Command, CommandGroup, Group } from "./definitions";
 
 @Group("CTF")
@@ -106,5 +107,62 @@ export default class CTF extends CommandGroup {
       .run(
         async ({ ctfChannel }) => `Checkout ${ctfChannel} for more information!`
       );
+  }
+
+  @Command({
+    desc: "Add credentials to the pinned CTFBot message",
+    usage: "!addcreds field1=val1 field2=val2..."
+  })
+  public async addcreds(ctx: CmdCtx) {
+    const newCreds = ctx.args
+      .map(x => x.split("="))
+      .filter(x => x.length === 2)
+      .filter(x => x[0].indexOf("```") === -1 && x[1].indexOf("```") === -1);
+
+    const ctf = await getCTFTimeCTF(ctx);
+    for (const [key, val] of newCreds) {
+      ctf.credentials[key] = val;
+    }
+    await ctf.save();
+
+    const mainMessage = await getCtfMainEmbed(ctx, ctf);
+    await mainMessage.edit(ctfMainEmbed(ctf));
+    ctx.send(success(`Credentials added`));
+  }
+
+  @Command({
+    desc: "Remove credentials from the pinned CTFBot message",
+    usage: "!rmvcreds field1 field2 ..."
+  })
+  public async rmvcreds(ctx: CmdCtx) {
+    const ctf = await getCTFTimeCTF(ctx);
+
+    for (const key of ctx.args) {
+      delete ctf.credentials[key];
+    }
+    await ctf.save();
+
+    const mainMessage = await getCtfMainEmbed(ctx, ctf);
+    await mainMessage.edit(ctfMainEmbed(ctf));
+    ctx.send(success(`Credentials removed`));
+  }
+
+  // TODO ref the archive-hats Trello user idea
+  @Command({
+    desc: "Archive a CTF"
+  })
+  public async archive(ctx: CmdCtx) {
+    const ctf = await getCTFTimeCTF(ctx);
+
+    const archive = ctx.msg.guild.channels.find(x => x.name === "archives");
+    if (!archive) {
+      throw new CommandError("CTFs archive channel missing");
+    }
+    await ctx.textChannel.setParent(archive);
+
+    ctf.archived = true;
+    await ctf.save();
+
+    ctx.send(success(`Archived!`));
   }
 }
